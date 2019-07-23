2c85283b82f63797b0dd87e913a1e9c431344169
pexcn <i@pexcn.me>
2019-07-23 23:36:13 +0800

    Update fix.sh

--- fix/nvidia/fix.sh
+++ fix/nvidia/fix.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
 curl -sSL https://github.com/pexcn/share/raw/master/fix/nvidia/nvidia-card-disabler.service > /lib/systemd/system/nvidia-card-disabler.service
-curl -sSL https://github.com/pexcn/share/raw/master/fix/nvidia/nvidia-card-enabler.service > /lib/systemd/system/nvidia-card-enabler.service
