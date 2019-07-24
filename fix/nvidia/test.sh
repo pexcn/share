c6d5a30673837b8f48c40d7dfcaf89854b1ece1e
pexcn <i@pexcn.me>
2019-07-25 00:22:07 +0800

    Delete test.sh

--- fix/nvidia/test.sh
+++
@@ -1,8 +0,0 @@
-# disable
-echo 1 > /sys/bus/pci/devices/0000:01:00.0/remove
-echo 1 > /sys/bus/pci/devices/0000:01:00.1/remove
-echo '\\_SB.PCI0.PEG0.PEGP._OFF' > /proc/acpi/call
-
-# enable
-echo '\\_SB.PCI0.PEG0.PEGP._ON' > /proc/acpi/call
-echo 1 > /sys/bus/pci/rescan
